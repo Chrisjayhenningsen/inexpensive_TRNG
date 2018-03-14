c9feb609e950fe6abac4e4fd83682c2b5a9e1720
christopher Henningsen <chrisjayhenningsen@gmail.com>
2018-03-14 17:43:06 -0300

    Allowed large datasets to be shared

--- app/src/main/java/com/choosemuse/example/libmuse/MainActivity.java
+++ app/src/main/java/com/choosemuse/example/libmuse/MainActivity.java
@@ -284,7 +284,7 @@ public class MainActivity extends Activity implements OnClickListener {
         startActivity(share_data);
 
     }
-
+//sincere thanks for all serendipity
 
     //--------------------------------------
     // Listeners
